be0a411024ce8822495e651e9c45e8e6ce037374
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-06-21 08:22:57 +0200

    Create node: for panel-heading(=> add button, title node)

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -6,9 +6,33 @@ function createNewNote() {
         return newNote;
     }
 
+    function newPanelHeading() {
+        function newTitle() {
+            const newTitle = document.createElement("h3");
+            newTitle.className = "panel-title";
+            newTitle.id = "title";
+            newTitle.setAttribute("contenteditable", "true");
+            newTitle.innerText = "Edit me!";
+            return newTitle;
+        }
+
+        function newButton() {
+            const newButton = document.createElement("button");
+            newButton.className = "button-close";
+            newButton.innerText = "x";
+            return newButton;
+        }
+
+        const newPanelHeading = document.createElement("div");
+        newPanelHeading.className = "panel-heading";
+        let title = newTitle();
+        newPanelHeading.appendChild(title);
+        let button = newButton();
+        newPanelHeading.appendChild(button);
+        return newPanelHeading;
+    }
 
 
-}
 
 function main() {
     let notepadIcon = document.querySelector("#notepad-icon");
